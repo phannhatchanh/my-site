d78a6c31a5c21ed7e4e3662e77f57b91519d1030
phannhatchanh <phannhatchanh@gmail.com>
2020-09-26 21:00:37 +0700

    enable Disqus comment system

--- _config.js
+++ _config.js
@@ -27,7 +27,7 @@ module.exports = {
 
   /** Post */
   enablePostOfContents: true,     
-  disqusShortname: '',  
+  disqusShortname: 'phannhatchanh',  
   enableSocialShare: true,        
 
   /** Optional */
