0954ad06761386df18c4e84eeae84334d8fa875b
phannhatchanh <phannhatchanh@gmail.com>
2021-02-20 21:37:56 +0700

    link reading

--- src/components/Bio/index.tsx
+++ src/components/Bio/index.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import { Link } from 'gatsby'
 import { FontAwesomeIcon as Fa } from '@fortawesome/react-fontawesome';
-import { faUserCircle, faAt, faMapMarkerAlt, faLink, faAddressCard, faRss, faEnvelope } from '@fortawesome/free-solid-svg-icons';
-import { faFacebook, faInstagram, faSketch, faLinkedin, faGithub, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faUserCircle, faAt, faMapMarkerAlt, faIdCard, faBookReader, faLink, faAddressCard, faRss, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faInstagram, faLinkedin, faGithub, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
 import './bio.scss';
 import config from '../../../_config';
 
 const Bio = () => {
-  const { comment, name, company, country, email, website, linkedin, facebook, instagram, github, youtube, about, contact, resume } = config;
+  const { comment, name, company, country, email, website, linkedin, facebook, instagram, github, youtube, about, contact, reading, resume } = config;
 
   return (
     <div className="bio">
@@ -54,21 +54,28 @@ const Bio = () => {
 
       {!about ? null : (
         <div className="bio-item">
-          <div className="icon-wrap"><Fa icon={faUserCircle} /></div>
+          <div className="icon-wrap"><Fa icon={faUserCircle} color="gray" /></div>
           <Link to="/about/" activeClassName="active">{about}</Link>
         </div>
       )}
       
       {!contact ? null : (
         <div className="bio-item">
-          <div className="icon-wrap"><Fa icon={faAt} /></div>
+          <div className="icon-wrap"><Fa icon={faAt} color="gray" /></div>
           <Link to="/contact/" activeClassName="active">{contact}</Link>
         </div>
       )}
       
+      {!reading ? null : (
+        <div className="bio-item">
+          <div className="icon-wrap"><Fa icon={faBookReader} color="gray" /></div>
+          <Link to="/reading/" activeClassName="active">{reading}</Link>
+        </div>
+      )}
+
       {!resume ? null : (
         <div className="bio-item">
-          <div className="icon-wrap"><Fa icon={faSketch} /></div>
+          <div className="icon-wrap"><Fa icon={faIdCard} color="gray" /></div>
           <Link to="/resume/" activeClassName="active">{resume}</Link>
         </div>
       )}
@@ -76,37 +83,37 @@ const Bio = () => {
       <div className="social">
         {!linkedin ? null : (
           <a href={linkedin} target="_blank" rel="noopener noreferrer">
-            <Fa icon={faLinkedin} className="linkedin" />
+            <Fa icon={faLinkedin} color="gray" className="linkedin" />
           </a>
         )}
 
         {!facebook ? null : (
           <a href={facebook} target="_blank" rel="noopener noreferrer">
-            <Fa icon={faFacebook} className="facebook" />
+            <Fa icon={faFacebook} color="gray" className="facebook" />
           </a>
         )}
         {!instagram ? null : (
           <a href={instagram} target="_blank" rel="noopener noreferrer">
-            <Fa icon={faInstagram} className="instagram" />
+            <Fa icon={faInstagram} color="gray" className="instagram" />
           </a>
         )}
         {!email ? null : (
           <a href={email} target="_blank" rel="noopener noreferrer">
-            <Fa icon={faEnvelope} className="email" />
+            <Fa icon={faEnvelope} color="gray" className="email" />
           </a>
         )}
         {!github ? null : (
           <a href={github} target="_blank" rel="noopener noreferrer">
-            <Fa icon={faGithub} className="github" />
+            <Fa icon={faGithub} color="gray" className="github" />
           </a>
         )}
         {!youtube ? null : (
           <a href={youtube} target="_blank" rel="noopener noreferrer">
-            <Fa icon={faYoutube} className="youtube" />
+            <Fa icon={faYoutube} color="gray" className="youtube" />
           </a>
         )}
         <a href={`${config.siteUrl}/rss.xml`} target="_blank" rel="noopener noreferrer">
-          <Fa icon={faRss} className="rss" />
+          <Fa icon={faRss} color="gray" className="rss" />
         </a>
       </div>
     </div>
